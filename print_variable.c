81947aa52e156e1688ee1a4eecb25a23e28acbe3
youchen11 <90672914+youchen11@users.noreply.github.com>
2021-12-07 13:49:17 +0800

    Create print_variable.c

---
+++ print_variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int a = 87;
+    for (int i = 5; i < 23; i++) {
+            printf("i=%d, a=%d\n", i, a*i);
+        //give me some output messages in following format
+        //and that can be done by add only ONE line code.
+        //please refer to Google slides, page 22.
+        //i=5, a=435
+        //i=6, a=522
+        //i=7, a=609
+        //i=8, a=696
+        //i=9, a=783
+        //...
+        //i=22, a=1914
+    }
+}
