f2d499d20bd90e7253912f504e37b7b69a949ae8
youchen11 <90672914+youchen11@users.noreply.github.com>
2021-12-21 13:53:41 +0800

    Update fetch_input.c

--- fetch_input.c
+++ fetch_input.c
@@ -14,8 +14,10 @@ int main() {
     //only ONE line code is you need
     //please refer page 18 of slides
     scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
-        printf("a=%d\n", a);
-        printf("b=%d\n", b);
-        printf("c=%d\n", c);
-        printf("d=%d\n", d);
-        printf("e=%d\n", e);
+
+    printf("a=%d\n", a);
+    printf("b=%d\n", b);
+    printf("c=%d\n", c);
+    printf("d=%d\n", d);
+    printf("e=%d\n", e);
+}
